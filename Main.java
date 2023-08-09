9d7a080b7c91f7113f336d6622fa7c7516e33684
Phiniqq724 <141791615+Phiniqq724@users.noreply.github.com>
2023-08-09 08:45:09 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,8 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
 
-        int jumlahpembelian, soto, ayamgoreng, babiguling, baksosetan, sateunta, nasi, plastik, totalharga;
+        int jumlahpembelian, soto, ayamgoreng, babiguling, baksosetan, sateunta, nasi, plastik, totalharga, sempolcicak, pudinglele, miegoreng, nasigorengmaut, sayurkangtheconqueror;
         String beli;
+
         soto = 10000;
         ayamgoreng = 12000;
         babiguling = 48000;
@@ -16,6 +17,12 @@ public class Main {
         sateunta = 50000;
         nasi = 5000;
         plastik = 1000;
+        pudinglele = 25000;
+        sempolcicak = 2500;
+        miegoreng = 7000;
+        nasigorengmaut = 90000;
+        sayurkangtheconqueror = 9999999;
+
 
         System.out.print("Beli apa? ");
         beli = myObj.nextLine();
@@ -47,6 +54,33 @@ public class Main {
             System.out.println("Kamu membeli = Sate Unta");
             System.out.println("Harga Soto = " + sateunta*jumlahpembelian);
             totalharga = sateunta*jumlahpembelian;
+        
+        }
+        if("sempolcicak".equals(beli)) {
+            System.out.println("Kamu membeli = Sempol Cicak");
+            totalharga = sempolcicak * jumlahpembelian;
+            System.out.println("Total harga = " + totalharga);
         }         
+        if("pudinglele".equals(beli)) {
+            System.out.println("Kamu membeli = Puding Lele");
+            totalharga = pudinglele * jumlahpembelian;
+            System.out.println("Total harga = " + totalharga);
+        }         
+        if("miegoreng".equals(beli)) {
+            System.out.println("Kamu membeli = Mie Goreng");
+            totalharga = miegoreng * jumlahpembelian;
+            System.out.println("Total harga = " + totalharga);
+        }         
+        if("nasgor".equals(beli)) {
+            System.out.println("Kamu membeli = Nasi Goreng Maut");
+            totalharga = nasigorengmaut * jumlahpembelian;
+            System.out.println("Total harga = " + totalharga);
+        }         
+        if("sayurkangkung".equals(beli)) {
+            System.out.println("Kamu membeli = Sayur Kang The Conqueror");
+            totalharga = sayurkangtheconqueror * jumlahpembelian;
+            System.out.println("Total harga = " + totalharga);
+        }         
+        
     }
 }
